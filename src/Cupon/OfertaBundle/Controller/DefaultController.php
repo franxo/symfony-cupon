4e6ea7facfb8c21f337921ae83ac659453cb8c65
franxo <franciscogarde@gmail.com>
2015-02-28 03:21:15 +0100

    Create first static example page 'Ayuda'

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function ayudaAction()
     {
-        return $this->render('OfertaBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('OfertaBundle:Default:ayuda.html.twig');
     }
 }
